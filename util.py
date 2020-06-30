f9930d721a553c8bd7f158292c478e1240adcfda
Jonas Lieb <lieb.jonas@gmail.com>
2020-06-30 19:50:52 +0200

    Reformat with black and isort

--- util.py
+++ util.py
@@ -1,6 +1,6 @@
-def hex2bytes( hex ):
+def hex2bytes(hex):
     numbers = []
-    for i in range( 0, len(hex), 2 ):
-        pair = hex[i:i+2]
+    for i in range(0, len(hex), 2):
+        pair = hex[i : i + 2]
         numbers.append(int(pair, 16))
     return bytes(numbers)
